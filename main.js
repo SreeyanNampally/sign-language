f281ef9716b2b20732bb47452db3e0553fcf4277
Sreeyan Nampally <98562308+SreeyanNampally@users.noreply.github.com>
2023-08-24 15:37:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+prediction = "";
+
+Webcam.attach("#camera")
+camera= document.getElementById("camera")
+Webcam.set({
+    width:350,
+    height:270,
+    image_format:"png",
+    png_quality:100
+})
+
